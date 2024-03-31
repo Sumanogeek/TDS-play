ddf15d9b066007efe46059c18640b3086957a446
Sumanogeek <sumanogeek@gmail.com>
2024-03-31 19:51:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import streamlit as st
 
-def addition(a,b):
-  return (a+b)
+def calc(a,b,op):
+  if op == '+':
+    return (a+b)
+  elif op == '-':
+    return (a-b)
+  elif op == '*':
+    return (a*b)
+  elif op == '/':
+    return (a/b)
+  return ('operation not listed')
+    
 
 st.title("Model Caclulator")
 a = st.number_input('Insert 1st number: ')
 b = st.number_input('Insert 2nd number: ')
+op = st.text_input('Insert operation (+ or - or * or / ): ')
 
-value = addition(a,b)
+value = calc(a,b,op)  
 st.write(value)
