65bb7ecc82574c8eb19afc2113efbe636745781d
Sumanogeek <sumanogeek@gmail.com>
2024-03-31 19:38:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print ("Hello IITM")
+import streamlit as st
+
+def addition(a,b):
+  return (a+b)
+
+st.title("Model Caclulator")
+a = st.input("enter a: ")
+b = st.input("enter b: ")
+
+value = addition(a,b)
+st.write(value)
