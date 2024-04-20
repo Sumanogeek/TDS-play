5f637acdeb02b131911af165c5296abb68560400
Sumanogeek <sumanogeek@gmail.com>
2024-04-20 21:47:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 import streamlit as st
 
-def calc(a,b,op):
-  if op == '+':
-    return (a+b)
-  elif op == '-':
-    return (a-b)
-  elif op == '*':
-    return (a*b)
-  elif op == '/':
-    return (a/b)
-  return ('operation not listed')
+def calc(a,b,c):
+  if a > b:
+    if a > c:
+      return a
+    else:
+      return c
+  else:
+    if b > c:
+      return b
+    else:
+      return c
     
-
-st.title("Model Caclulator")
+st.title("Find Largest")
 a = st.number_input('Insert 1st number: ')
 b = st.number_input('Insert 2nd number: ')
-op = st.text_input('Insert operation (+ or - or * or / ): ')
+c = st.number_input('Insert 3rd number: ')
+# op = st.text_input('Insert operation (+ or - or * or / ): ')
 
-value = calc(a,b,op)  
+value = calc(a,b,c)  
 st.write(value)
